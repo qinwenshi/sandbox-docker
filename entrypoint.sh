6bc42132359d5ad1675c20283055a3b5dcd3b595
Wen <qinwenshi@users.noreply.github.com>
2017-09-10 12:06:34 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 # if command starts with an option, prepend sandbox run
 if [ "${1:0:1}" = '-' ]; then
-  set -- ./sandbox run "$@"
-fi
-
-if [ "$1" = './sandbox' ]; then
-  echo "Starting sandbox"
-  exec /gosu sandbox "$@"
+  set -- java -jar /usr/bin/sandbox run "$@"
 fi
 
 echo "Passthrough command"
